b1034838ccfe1ac2191e45600f7eb0bf690ee15c
aleRiki <alericardo826@gmail.com>
2025-12-01 02:42:05 +0100

    comit

--- constants/config.ts
+++ constants/config.ts
@@ -1,10 +1,12 @@
-import { Platform } from 'react-native';
 
-const getBaseUrl = () => {
-  if (Platform.OS === 'android') {
-    return 'http://10.0.2.2:3001';
-  }
-  return 'http://localhost:3001';
-};
+// Production backend URL
+export const API_URL = 'https://backed-economic.onrender.com';
 
-export const API_URL = getBaseUrl();
+// For local development, comment the line above and uncomment the lines below:
+// const getBaseUrl = () => {
+//   if (Platform.OS === 'android') {
+//     return 'http://10.0.2.2:3001';
+//   }
+//   return 'http://localhost:3001';
+// };
+// export const API_URL = getBaseUrl();
