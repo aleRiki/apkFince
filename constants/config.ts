0649cf2aaa02717c873c94397ddb4606eca574c1
aleRiki <alericardo826@gmail.com>
2025-12-02 16:01:35 +0100

    comit

--- constants/config.ts
+++ constants/config.ts
@@ -1,7 +1,7 @@
 
 // Production backend URL
-export const API_URL = 'https://backed-economic.onrender.com';
-
+export const API_URL = 'http://10.0.2.2:3001';
+//'https://backed-economic.onrender.com'
 // For local development, comment the line above and uncomment the lines below:
 // const getBaseUrl = () => {
 //   if (Platform.OS === 'android') {
